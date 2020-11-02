40a01bd9cda704de48666425487c3ac442dc0b73
Gabriel S <desouza.gabriel@icloud.com>
2020-11-02 19:54:41 -0300

    feat(services/weather): added service for ropenweathermap gem
    add service to interact with ropenweathermap gem

---
+++ app/services/weather_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class WeatherService
+  attr_accessor :client, :city
+
+  def initialize(api_key:)
+    self.client = Ropenweather::Client.new(api_key: api_key, lang: 'pt_BR')
+  end
+
+  def get_weather(city:)
+    self.city = city
+    { today: current, forecasts: forecasts }
+  end
+
+  private
+
+  def current
+    response = client.current_weather(city: city)
+    Weather.new(date: response[:dt_txt],
+                temp: response[:main][:temp],
+                description: response[:weather].first[:description])
+  end
+
+  def forecasts
+    response = client.current_weather(city: city, action: 'forecast')
+
+    averages = []
+    time_gap.each do |date|
+      filtered_list = response[:list].select { |item| item[:dt_txt].to_date == date }
+
+      averages << Weather.new(date: date,
+                              temp: daily_average(filtered_list))
+    end
+    averages
+  end
+
+  def time_gap
+    start = 1.day.since.to_date
+    (start..5.days.since).to_a
+  end
+
+  def daily_average(list)
+    temperatures = list.map { |item| item[:main][:temp] }
+
+    (temperatures.sum(0.0) / temperatures.size).round
+  end
+end
